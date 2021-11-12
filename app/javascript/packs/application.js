e9efed6c842fadf955cd99bf0d06543364c9d76d
Galym Parpiyev <parpiyev.galym@protonmail.com>
2021-11-12 11:14:15 +0900

    pre-deployment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap';
-
+import 'owl.carousel/dist/assets/owl.carousel.css';
+import 'owl.carousel';
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
@@ -17,3 +18,8 @@ import 'bootstrap';
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel();
+});
+
